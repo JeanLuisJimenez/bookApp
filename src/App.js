e08bd1a93976effaeb19a9f9c81964dcbd636c9e
JeanLuisJimenez <Jean_Luis_Jimenez@intellisys.com.do>
2019-04-16 17:04:24 -0400

    Nada'

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
 import React, { Component } from 'react';
-import { setData, setDataPlaces } from './api';
+import { setDataPlaces, setDataActivities, deletePlaces, deleteActivities } from './api';
 import './App.css';
 
 class App extends Component {
     render () {
         return (
-            <div className="App">
-                <button onClick={setData}>
-                    Set data books
-                </button>
-                {/*<button onClick={setDataEvents}>*/}
-                    {/*Set data events*/}
-                {/*</button>*/}
-                <button onClick={setDataPlaces}>
-                    Set data places
-                </button>
+            <div className="App" style={{flexDirection: 'row'}}>
+                {/*<button onClick={setData}>*/ }
+                {/*    Set data books*/ }
+                {/*</button>*/ }
+                {/*<button onClick={setDataEvents}>*/ }
+                {/*Set data events*/ }
+                {/*</button>*/ }
+                <div style={{alignContent: 'center'}}>
+                    <h2> Places </h2>
+                    <button onClick={ setDataPlaces }>
+                        Set data places
+                    </button>
+                    <button onClick={ deletePlaces }>
+                        Delete data places
+                    </button>
+
+                </div>
+                <div style={{alignContent: 'center'}}>
+                    <h2> Activities </h2>
+                    <button onClick={ setDataActivities }>
+                        Set data Activities
+                    </button>
+                    <button onClick={ deleteActivities }>
+                        Delete data Activities
+                    </button>
+                </div>
             </div>
         );
     }
