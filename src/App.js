edf1f45b04da6e03a23a99c664b8489f5bb16a8e
JeanLuisJimenez <Jean_Luis_Jimenez@intellisys.com.do>
2019-04-18 11:52:29 -0400

    Creating methods for instert any data of books

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
 import React, { Component } from 'react';
-import { setDataPlaces, setDataActivities, deletePlaces, deleteActivities } from './api';
+import { deleteActivities, deletePlaces, removeBooks, setBooks, setDataActivities, setDataPlaces } from './api';
 import './App.css';
+import axios from 'axios';
+import data from './data/libros';
 
 class App extends Component {
+
+    state = {
+        books: [],
+    };
+
+    getDataGoogleBooks = async () => {
+        let counter = 0;
+        while (counter < data.length - 3530) {
+            await data.slice(counter, counter + 10).map((book, index) => {
+                let bookData = axios.get(`https://www.googleapis.com/books/v1/volumes?q=${ encodeURI(book) }&key=AIzaSyBoT1SaD59FTZ8Nv0iCFilpDk_FZq_vRxw`)
+                    .then(res => res.data.items);
+                return bookData.then(async res => {
+                        let books = this.state.books;
+                        books.push({
+                            titulo: book.TITULO,
+                            categoria: book.GENERO || res[ 0 ].volumeInfo.categories[ 0 ] || '',
+                            autor: book.AUTOR || res[ 0 ].volumeInfo.authors[ 0 ] || '',
+                            precio: book.PVP,
+                        });
+                        await this.setState({books: books})
+                    },
+                );
+            });
+            counter += await 10;
+            await setTimeout(() => console.log('Waiting for response'), 1000);
+        }
+        console.log(this.state.books);
+        setBooks(this.state.books).then(res => console.log(res)).catch(e => e);
+    };
+
     render () {
         return (
-            <div className="App" style={{flexDirection: 'row'}}>
-                {/*<button onClick={setData}>*/ }
-                {/*    Set data books*/ }
-                {/*</button>*/ }
-                {/*<button onClick={setDataEvents}>*/ }
-                {/*Set data events*/ }
-                {/*</button>*/ }
-                <div style={{alignContent: 'center'}}>
+            <div className="App" style={ {flexDirection: 'row'} }>
+                <div style={ {alignContent: 'center'} }>
                     <h2> Places </h2>
                     <button onClick={ setDataPlaces }>
                         Set data places
@@ -22,7 +48,7 @@ class App extends Component {
                     </button>
 
                 </div>
-                <div style={{alignContent: 'center'}}>
+                <div style={ {alignContent: 'center'} }>
                     <h2> Activities </h2>
                     <button onClick={ setDataActivities }>
                         Set data Activities
@@ -30,6 +56,11 @@ class App extends Component {
                     <button onClick={ deleteActivities }>
                         Delete data Activities
                     </button>
+                </div>
+                <div>
+                    <h2> Books </h2>
+                    <button onClick={ this.getDataGoogleBooks }>Set books</button>
+                    <button onClick={ removeBooks }>Delete books</button>
                 </div>
             </div>
         );
